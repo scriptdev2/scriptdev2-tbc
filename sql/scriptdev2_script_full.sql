13bfb221253737dad1f6882e706ce83846ed8227
Schmoozerd <schmoozerd@scriptdev2.com>
2012-12-21 10:10:54 +0200

    [s2662] Support hard-mode loot in Zul'Aman - Also fix compile
    * Fix compile with C-MaNGOS 13005, Requires s1845
    * Add support for hard-mode checks in Zul'Aman, timed event
     - Each chest can now be checked with 1..4 if it's associated npc was resued first..forth (or 0 for none at all)
    
    (based on commit 7d5cff4)

--- sql/scriptdev2_script_full.sql
+++ sql/scriptdev2_script_full.sql
@@ -3,7 +3,7 @@
 --
 
 DELETE FROM sd2_db_version;
-INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for C-MaNGOS s1833+) ');
+INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for C-MaNGOS s1845+) ');
 
 --
 -- Below contains data for table `script_texts` mainly used in C++ parts.
