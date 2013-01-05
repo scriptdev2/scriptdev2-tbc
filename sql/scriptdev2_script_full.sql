c21d268d1f145fd3739d860613eacdd6558ae139
Schmoozerd <schmoozerd@scriptdev2.com>
2013-01-05 14:15:51 +0200

    [s2666] Use Unit::RemoveAllAurasOnEvade. Requires C-MaNGOS [s1858]
    (based on commit [2779] - 04d2efe)

--- sql/scriptdev2_script_full.sql
+++ sql/scriptdev2_script_full.sql
@@ -3,7 +3,7 @@
 --
 
 DELETE FROM sd2_db_version;
-INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for C-MaNGOS s1846+) ');
+INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for C-MaNGOS s1858+) ');
 
 --
 -- Below contains data for table `script_texts` mainly used in C++ parts.
