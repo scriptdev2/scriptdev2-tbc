18caa3080c428bbfca43fb30704fa00b5c0c2805
Xfurry <xfurry@scriptdev2.com>
2013-02-24 01:35:14 +0200

    [s2671] Add script support for SSC water. Requires CMangos s1894
    (based on commit 1385b30)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- sd2_revision_nr.h
+++ sd2_revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __SD2_REVISION_NR_H__
 #define __SD2_REVISION_NR_H__
- #define SD2_REVISION_NR "s2670"
+ #define SD2_REVISION_NR "s2671"
 #endif // __SD2_REVISION_NR_H__
--- sql/scriptdev2_script_full.sql
+++ sql/scriptdev2_script_full.sql
@@ -3,7 +3,7 @@
 --
 
 DELETE FROM sd2_db_version;
-INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for C-MaNGOS s1866+) ');
+INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for C-MaNGOS s1894+) ');
 
 --
 -- Below contains data for table `script_texts` mainly used in C++ parts.
