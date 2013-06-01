b4c77d497130180a272ca93188cf63dd403d99f5
Schmoozerd <schmoozerd@scriptdev2.com>
2013-06-01 20:30:27 +0300

    [s2732] Add AIEvent throwing wrappers. Requires CMaNGOS [s2006]
    Also use these Events to start an escort script as example
    
    (based on commit 921d915)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- sql/scriptdev2_script_full.sql
+++ sql/scriptdev2_script_full.sql
@@ -3,7 +3,7 @@
 --
 
 DELETE FROM sd2_db_version;
-INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for CMaNGOS s1989+) ');
+INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for CMaNGOS s2006+) ');
 
 --
 -- Below contains data for table `script_texts` mainly used in C++ parts.
