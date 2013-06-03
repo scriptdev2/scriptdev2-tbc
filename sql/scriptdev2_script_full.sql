c30c4b7bfff418b92db573e7807a27b194ef5986
Schmoozerd <schmoozerd@scriptdev2.com>
2013-06-03 18:06:32 +0200

    [s2747] Update to CMaNGOS [s2036]
    This requires and fixes compile for CMaNGOS [s2036]
    
    Also use the generic text storage now to load the data and display the texts
    
    Signed-off-by: Schmoozerd <schmoozerd@scriptdev2.com>

--- sql/scriptdev2_script_full.sql
+++ sql/scriptdev2_script_full.sql
@@ -3,7 +3,7 @@
 --
 
 DELETE FROM sd2_db_version;
-INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for CMaNGOS s2026+) ');
+INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for CMaNGOS s2036+) ');
 
 --
 -- Below contains data for table `script_texts` mainly used in C++ parts.
