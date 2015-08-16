378b22a1d2c26b01b0769a6ef075697eeabb3848
xfurry <xfurry@scriptdev2.com>
2015-08-16 12:22:28 +0300

    [s2854] Add missing backporting scripts

---
+++ tool/backporting/update-commit-log.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Simple helper script to create SD2-Backporting Lists
+
+# By user defined (remote/branch to the to-be-backported history)
+COMPARE_PATH="origin-wotlk/master"
+TODO_FILE="tool/backporting/todo_wotlk_commits.log"
+COMPARE_NAME="WotLK"
+
+# param1 must be the commit hash of last backported commit (of original commit)
+if [ "$#" != "1" ]
+then
+  echo "You must provide the last commit's hash of the \"$TODO_FILE\" file"
+  exit 1
+fi
+
+# are we in git root dir?
+if [[ ! -d .git/ ]]; then
+  echo "ERROR: This script is expected to be called from repository root directory"
+  echo "Try: tool/backporting/update-commit-log.sh"
+  exit 1
+fi
+
+HASH=$1
+
+git log $HASH..$COMPARE_PATH --pretty=format:"${COMPARE_NAME}_COMMIT %h  by %an (commiter %cn) \" %s \"" --reverse --dirstat >> $TODO_FILE
+echo "" >> $TODO_FILE
+echo "FILE LAST UPDATE BASED ON $(git log -1 --pretty="%h (by %an) \" %s \"" $COMPARE_PATH)" >> $TODO_FILE
+echo "" >> $TODO_FILE
